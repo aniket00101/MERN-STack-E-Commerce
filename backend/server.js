dc5c292654a0a84c9d0043b1333d50e623e1db37
aniket00101 <dasaniket971@gmail.com>
2026-03-04 11:24:29 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,46 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const app = express()
 
-dotenv.config();
+const connectDB = require("./config/db.js")
+const userRoutes = require("./routes/userRoutes.js")
+const productRoutes = require("./routes/ProductRoutes.js")
+const cartRoutes = require("./routes/cartRoutes.js")
+const checkoutRoutes = require("./routes/checkoutRoutes.js")
+const orderRoutes = require("./routes/orderRoutes.js")
+const uploadRoutes = require("./routes/uploadRoutes.js")
+const subscribedRoutes = require("./routes/subscriberRoutes.js")
+const adminRoutes = require("./routes/adminRoutes.js")
+const productAdminRoute = require("./routes/ProductRoutes.js")
+const adminorderRoute = require("./routes/adminorderRoutes.js")
 
-const connectDB = require("./config/db.js");
+app.use(express.json())
+app.use(cors())
+dotenv.config()
 
-const userRoutes = require("./routes/userRoutes.js");
-const productRoutes = require("./routes/ProductRoutes.js");
-const cartRoutes = require("./routes/cartRoutes.js");
-const checkoutRoutes = require("./routes/checkoutRoutes.js");
-const orderRoutes = require("./routes/orderRoutes.js");
-const uploadRoutes = require("./routes/uploadRoutes.js");
-const subscribedRoutes = require("./routes/subscriberRoutes.js");
-const adminRoutes = require("./routes/adminRoutes.js");
-const productAdminRoute = require("./routes/ProductRoutes.js");
-const adminorderRoute = require("./routes/adminorderRoutes.js");
+const port = process.env.PORT || 3000
 
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-// Connect DB BEFORE routes
-connectDB();
+connectDB()
 
 app.get("/", (req, res) => {
-  res.send("Welcome to NextMart API");
+    res.send("Welcome to NextMart API");
 });
 
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/checkout", checkoutRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("/api/subscribe", subscribedRoutes);
-app.use("/api/admin/users", adminRoutes);
-app.use("/api/admin/products", productAdminRoute);
-app.use("/api/admin/orders", adminorderRoute);
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/cart", cartRoutes)
+app.use("/api/checkout", checkoutRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
+app.use("/api/subscribe", subscribedRoutes)
+app.use("/api/admin/users", adminRoutes)
+app.use("/api/admin/products", productAdminRoute)
+app.use("/api/admin/orders", adminorderRoute)
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
+// username: dasaniket971_db_user
+// password: 2APWq440WmWW9Kn1
